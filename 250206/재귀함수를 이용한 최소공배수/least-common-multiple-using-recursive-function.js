16efa341e798c48cc508e13b2176ff921fbdcac3
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-02-06 14:51:21 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 181ms, Memory: 15MB, Status: Passed - CodeTree

--- 250206/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.js
+++ 250206/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.js
@@ -6,18 +6,25 @@ const arr = input[1].split(' ').map(Number);
 
 console.log(recur(1,0))
 
-function recur(acc ,depth){
+
+function gcd(a,b){
+    if(b === 0) return a;
+    return a > b ? gcd(b , a % b) : gcd(a, b % a);
+}
+
+function lcm(a,b){
+    return a * b / gcd(a,b);
+}
+
+function recur(prev ,depth){
     if(depth === arr.length - 1){
-        const current = arr[depth]
-        if(acc % current === 0) return acc;
-        else return acc * current;
+        const current = arr[depth];
+
+        return lcm(prev, current)
     }
 
     const current = arr[depth];
+    const currentLcm = lcm(prev, current)
 
-    if(acc % current === 0){
-        return recur(acc, depth + 1);
-    }else{
-        return recur(acc * current, depth + 1);
-    }
+    return recur(currentLcm,depth+1)
 }
