731b4aaf4e9ccd94a8a49971c315cc35bc9eac05
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-02-06 15:46:17 +0900

    Title: k번째로 신기한 문자열, Time: 172ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250206/k번째로 신기한 문자열/kth-special-string.js
+++ 250206/k번째로 신기한 문자열/kth-special-string.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, k, T] = input[0].trim().split(' ');
 
-const words = input.slice(1).map(str=>str.trim()).filter(str=>str.includes(T)).sort();
+const words = input.slice(1).map(word=>word.trim()).filter(str=>str.includes(T)).sort();
 
 
-console.log(words[k-1].trim())
+console.log(words[Number(k)-1])
