45139e452a729401d96dadb7e675dcc9ac04b289
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-03-11 19:30:03 +0900

    Title: 왔다 갔던 구역 2, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

--- 250311/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 250311/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-const commands = input.slice(1, n + 1);
+const commands = input.slice(1, n + 1).map(str => str.split(' '));
 
 const arr = Array.from({length:2001},()=>0);
 
@@ -14,18 +14,21 @@ for(const [val, dir] of commands){
   const num = Number(val)
 
   if(dir === 'R'){
-    for(let i = cursor ; i < cursor + val ; i++){
+    for(let i = cursor ; i < cursor + num ; i++){
       arr[i]++
     }
+    cursor += num;
+
   }else{
-    for(let i = cursor - 1 ; i >= cursor - val ; i--){
+    for(let i = cursor - 1 ; i >= cursor - num ; i--){
       arr[i]++
     }
+    cursor -= num;
   }
 }
 
 
-for(let i = 1 ; i < arr.length ; i++){
+for(let i = 0 ; i < arr.length ; i++){
   if(arr[i] >= 2) ans++;
 }
 
