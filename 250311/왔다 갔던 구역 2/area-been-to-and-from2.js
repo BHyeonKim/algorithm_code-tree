78ce894982aba9fe9ca40ee1e8eaa24965a8dff7
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-03-11 18:25:26 +0900

    Title: 왔다 갔던 구역 2, Time: 185ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250311/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const commands = input.slice(1, n + 1);
+
+const arr = Array.from({length:2001},()=>0);
+
+let cursor = 1000;
+
+let ans = 0;
+
+for(const [val, dir] of commands){
+  const num = Number(val)
+
+  if(dir === 'R'){
+    for(let i = cursor ; i < cursor + val ; i++){
+      arr[i]++
+    }
+  }else{
+    for(let i = cursor ; i > cursor - val ; i--){
+      arr[i]++
+    }
+  }
+}
+
+for(let i = 0 ; i < arr.length ; i++){
+  if(arr[i] >= 2) ans++;
+}
+
+console.log(ans)
