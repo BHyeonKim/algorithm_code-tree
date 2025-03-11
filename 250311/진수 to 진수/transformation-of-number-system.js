cf1a7ace721c3e34c40f26c77d64aea9e37a1c08
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-03-11 16:45:54 +0900

    Title: 진수 to 진수, Time: 180ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250311/진수 to 진수/transformation-of-number-system.js
+++ 250311/진수 to 진수/transformation-of-number-system.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [a, b] = input[0].split(' ').map(Number);
-const n = input[1];
-// Please Write your code here.
+const n = +input[1];
 
-function decimalToNth(decimal, n){
+console.log(decimalToConverted(convertedToDecimal(n, a), b))
+
+
+
+function decimalToConverted(decimal, n){
     let converted = '';
 
     while(decimal !== 0){
@@ -16,5 +19,12 @@ function decimalToNth(decimal, n){
 }
 
 function convertedToDecimal(convertedNum, n){
+    const num = String(convertedNum);
     let decimal = 0;
+
+    for(let i = num.length - 1 ; i >= 0 ; i--){
+        decimal += num[i] * Math.pow(n, i);
+    }
+
+    return Number(decimal);
 }
