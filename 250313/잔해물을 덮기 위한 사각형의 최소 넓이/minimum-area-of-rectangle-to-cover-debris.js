1f89aa0c77e62f7476459bba33e6498e7405d125
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-03-13 17:54:47 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 684ms, Memory: 60MB, Status: Runtime Error - CodeTree

---
+++ 250313/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const rect1 = input[0].split(' ').map(Number);
+const rect2 = input[1].split(' ').map(Number);
+
+const grid = Array.from({length:2000}, ()=> Array.from({length:2000},()=>0));
+
+const [x1,y1, x2, y2] = rect1;
+
+for(let x = x1 ; x <= x2 ; x++){
+    for(let y = y1 ; y <= y2 ; y++){
+        grid[x][y] = 1;
+    }
+}
+
+const [x_1,y_1, x_2, y_2] = rect2;
+
+for(let x = x_1 ; x <= x_2 ; x++){
+    for(let y = y_1 ; y <= y_2 ; y++){
+        grid[x][y] = 0;
+    }
+}
+
+
+let leftBound = Number.POSITIVE_INFINITY;
+let bottomBound = Number.POSITIVE_INFINITY
+let rightBound = Number.NEGATIVE_INFINITY;
+let topBound = Number.NEGATIVE_INFINITY;
+
+for(let x = x1 ; x <= x2 ; x++){
+    for(let y = y1 ; y <= y2 ; y++){
+        if(grid[x][y]){
+            leftBound = Math.min(leftBound, x)
+            bottomBound = Math.min(bottomBound , y);
+            rightBound = Math.max(rightBound ,x);
+            topBound = Math.max(topBound, y);
+        }
+    }
+}
+
+
+console.log((rightBound - leftBound) * (topBound - bottomBound))
