947e6391767a857770e464134f3312deadf74be0
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-03-13 16:53:45 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 729ms, Memory: 61MB, Status: Runtime Error - CodeTree

---
+++ 250313/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [aX1,aY1,aX2,aY2] = input[0].split(' ').map(v=>Number(v));
+const [bX1,bY1,bX2,bY2] = input[1].split(' ').map(v=>Number(v));
+const [mX1,mY1,mX2,mY2] = input[2].split(' ').map(v=>Number(v));
+
+const grid = Array.from({length:2000},()=>Array.from({length:2000},()=>0));
+
+const lx = Math.min(aX1 , bX1);
+const rx = Math.max(aX2, bX2);
+const by = Math.min(aY1, bY1);
+const ty = Math.max(aY2, bY2);
+
+for(let i = aX1 ; i < aX2 ; i++){
+    for(let j = aY1 ; j < aY2 ; j++){
+        grid[i][j] = 1;
+    }
+}
+
+for(let i = bX1 ; i < bX2 ; i++){
+    for(let j = bY1 ; j < bY2 ; j++){
+        grid[i][j] = 1;
+    }
+}
+
+for(let i = mX1 ; i < mX2 ; i++){
+    for(let j = mY1 ; j < mY2 ; j++){
+        grid[i][j] = 2;
+    }
+}
+
+let ans = 0;
+
+for(let i = lx ; i < rx ; i++){
+    for(let j = by ; j < ty ; j++){
+        if(grid[i][j] === 1) ans++;
+    }
+}
+
+console.log(ans)
