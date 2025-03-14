23726ff05ebdace57bc469f70a43f84c36ee8c5e
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-03-15 02:18:45 +0900

    Title: 연속되는 수 2, Time: 162ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250315/연속되는 수 2/continuous-number2.js
+++ 250315/연속되는 수 2/continuous-number2.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input.slice(1, n + 1).map(Number);
 
-let ans = Number.NEGATIVE_INFINITY;
+let ans = 1;
 
 let prev = arr[0];
 let count = 1;
