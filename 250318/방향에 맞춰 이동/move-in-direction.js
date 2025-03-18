183f89bd39e5e6c82e54a895e4491bda8118716e
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-03-18 19:31:58 +0900

    Title: 방향에 맞춰 이동, Time: 183ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250318/방향에 맞춰 이동/move-in-direction.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const moves = input.slice(1).map(line=>line.split(' '));
+
+const ans = [0, 0]
+
+const delta = { W : [-1, 0], S : [0, -1], N : [0, 1], E : [1, 0]}
+
+for(const [d, distance] of moves){
+    ans[0] += delta[d][0] * distance
+    ans[1] += delta[d][1] * distance
+}
+
+console.log(ans.join(' '))
