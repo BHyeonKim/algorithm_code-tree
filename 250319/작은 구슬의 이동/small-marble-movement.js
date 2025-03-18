ddb00f32e9ac137597a47059ae75a087a66fd09b
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-03-19 00:01:05 +0900

    Title: 작은 구슬의 이동, Time: 167ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250319/작은 구슬의 이동/small-marble-movement.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [N, T] = input[0].split(' ').map(Number);
+let [R, C, Direction] = input[1].split(' ').map((value,inDirectionex)=> inDirectionex === 2 ? value : Number(value));
+
+const BOTTOM = 1;
+const TOP = N;
+const LEFT = 1;
+const RIGHT = N;
+
+
+
+
+if(Direction === 'R' || Direction === 'L'){
+    for(let time = 0 ; time < T ; time++){
+        if(Direction === 'R'){
+            if(C === RIGHT){
+                Direction = 'L'
+            }else{
+                C++;
+            }
+        }else{
+            if(C === LEFT){
+                Direction = 'R'
+            }else{
+                C--;
+            }
+        }
+    }
+}else{
+    for(let time = 0 ; time < T ; time++){
+        if(Direction === 'U'){
+            if(R === TOP){
+                Direction = 'D'
+            }else{
+                R++;
+            }
+        }else{
+            if(R === BOTTOM){
+                Direction  = 'U'
+            }else{
+                R--;
+            }
+        }
+    }
+}
+
+console.log(R+' '+C)
