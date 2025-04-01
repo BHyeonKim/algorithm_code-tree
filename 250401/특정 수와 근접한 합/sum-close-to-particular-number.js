9bcc1d826299283b1f3af905ee573da34f50959d
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-04-01 21:18:52 +0900

    Title: 특정 수와 근접한 합, Time: 165ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250401/특정 수와 근접한 합/sum-close-to-particular-number.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [_, S] = input[0].split(' ').map(Number);
+const numbers = input[1].split(' ').map(Number);
+
+const TOTAL = numbers.reduce((acc,val)=>acc+val,0)
+
+let closest = 0;
+
+for(let i = 0; i < numbers.length - 1 ; i++){
+    for(let j = 1 ; j < numbers.length ; j++){
+        const num = TOTAL - numbers[i] - numbers[j];
+
+        closest = Math.abs(S - num) > Math.abs(S - closest) ? closest : num
+    }
+}
+
+console.log(Math.abs(S - closest))
