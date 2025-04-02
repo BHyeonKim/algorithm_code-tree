b9d8a2a4cae1dca2e664085bfd6290fde2f7eddb
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-04-02 14:44:02 +0900

    Title: 구간 중 최대 합, Time: 187ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250402/구간 중 최대 합/max-sum-of-subarray.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [N, K] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+let sum = arr.slice(0, K).reduce((acc,val)=>acc+val,0)
+
+let ans = sum;
+
+for(let i = K ; i < N ; i++){
+    sum -= arr[i-K];
+    sum += arr[i];
+    ans = Math.max(ans, sum)
+}
+
+console.log(ans)
+
+
