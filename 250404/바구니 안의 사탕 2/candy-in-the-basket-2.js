79c3383d95a19213d52fc5c786a2b5cbb39d2537
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-04-04 18:44:51 +0900

    Title: 바구니 안의 사탕 2, Time: 167ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250404/바구니 안의 사탕 2/candy-in-the-basket-2.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n").map(line=>line.split(' ').map(Number));
+
+const [_, K] = input[0]
+const baskets = input.slice(1).sort((a,b)=>a[1]-b[1]);
+
+const  position = Array.from({length:101},()=>0)
+
+let leftBound = Number.POSITIVE_INFINITY;
+let rightBound = Number.NEGATIVE_INFINITY;
+
+let ans = 0;
+
+for(const [numOfCandy, coordinate] of baskets){
+    position[coordinate] = numOfCandy
+
+    rightBound = coordinate > rightBound ? coordinate : rightBound;
+    leftBound = coordinate < leftBound ? coordinate : leftBound;
+}
+
+
+for(let center = leftBound + K ; center <= rightBound - K ; center++){
+    let sum = 0
+    for(let j = center - K; j <= center + K ; j++){
+        sum += position[j]
+    }
+    ans = ans > sum ? ans : sum;
+}
+
+console.log(ans)
