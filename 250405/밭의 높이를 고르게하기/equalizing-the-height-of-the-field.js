1d861e44b8b0bdffffe06c46893ee4ab995371c9
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-04-05 15:22:53 +0900

    Title: 밭의 높이를 고르게하기, Time: 167ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250405/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
+++ 250405/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
@@ -6,18 +6,16 @@ const heights = input[1].split(' ').map(Number);
 
 let ans = Number.POSITIVE_INFINITY;
 
-for(let i = 0 ; i < N ; i++){
-    for(let j = i + T - 1 ; j < N ; j++){
-        let price = 0;
+for(let i = 0 ; i < N - T - 1 ; i++){
+    let price = 0;
 
-        for(let k = i ; k <= j ; k++){
-            const height = heights[k]
-            if(height === H) continue;
-            price += Math.abs(H - height)
-        }
-
-        ans = ans > price ? price : ans
+    for(let k = i ; k < i + T ; k++){
+        const height = heights[k]
+        if(height === H) continue;
+        price += Math.abs(H - height)
     }
+
+    ans = ans > price ? price : ans
 }
 
 console.log(ans)
