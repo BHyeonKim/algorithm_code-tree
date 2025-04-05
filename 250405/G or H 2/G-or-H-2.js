84c8f5e5e294a06660b4d75aa6d4ff476bc08f88
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-04-05 14:59:51 +0900

    Title: G or H 2, Time: 162ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250405/G or H 2/G-or-H-2.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const N = +input[0];
+const people = input.slice(1).map(line=>line.split(' ').map((val,idx)=>idx===0?+val:val)).sort((a,b)=>a[0]-b[0]);
+
+
+let ans = 0;
+
+
+for(let i = 0 ; i < people.length - 1 ; i++){
+    for(let j = i+1 ; j < people.length ; j++){
+        let g = 0, h = 0;
+
+        for(let k = i ; k <= j ; k++){
+            if(people[k][1] === 'G'){
+                g++;
+            }else{
+                h++;
+            }
+        }
+
+        if(g === h || !g || !h){
+            const length = people[j][0] - people[i][0];
+            ans = ans > length ? ans : length
+        }
+    }
+}
+
+console.log(ans)
