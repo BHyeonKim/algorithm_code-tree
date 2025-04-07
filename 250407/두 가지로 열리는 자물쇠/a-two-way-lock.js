7fa134364b70df0969e993067a1e22deedaf850a
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-04-07 15:28:41 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 192ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250407/두 가지로 열리는 자물쇠/a-two-way-lock.js
+++ 250407/두 가지로 열리는 자물쇠/a-two-way-lock.js
@@ -1,1 +1,32 @@
-const fs = require('fs')
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = +input[0];
+const [A1, B1, C1] = input[1].split(' ').map(Number);
+const [A2, B2, C2] = input[2].split(' ').map(Number);
+
+let ans = 0;
+
+for(let i = 1 ; i <= N ; i++){
+    for(let j = 1 ; j <= N ; j++){
+        for(let k = 1 ; k <= N ; k++){
+            if((isInRange(i, A1) && isInRange(j, B1) && isInRange(k, C1)) || (isInRange(i, A2) && isInRange(j, B2) && isInRange(k, C2))) ans++;
+        }
+    }
+}
+
+console.log(ans)
+
+function isInRange(a,b){
+    if(Math.abs(a-b) <= 2) return true;
+
+    if(a > b){
+        a = a - 9;
+    }else{
+        b = b - 9;
+    }
+
+    if(Math.abs(a-b) <= 1) return true;
+
+    return false;
+}
