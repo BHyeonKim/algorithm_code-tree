eb61702fb409aaa3ff563027c8f95a4a33440b89
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-04-12 23:47:00 +0900

    Title: 상해버린 치즈, Time: 177ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250412/상해버린 치즈/rotten-cheese.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(line=>line.split(' ').map(Number));
+
+const [N, M, D, S] = input[0]
+
+let eats = input.slice(1,1+D);
+const sicks = input.slice(1+D);
+
+const rottens = Array.from({length:N+1},()=>[]);
+
+
+for(const [personSick, sickTime] of sicks){
+    for(const [personAte, cheese, eatTime] of eats){
+        if(personSick !== personAte) continue;
+
+        if(sickTime <= eatTime) continue;
+
+        rottens[personAte].push(cheese)
+    }
+}
+
+const rottenCheck = Array.from({length:M+1},()=>true);
+rottenCheck[0] = false;
+
+for(let i = 1 ; i <= D ; i++){
+    for(let j = 1 ; j <= N ; j++){
+        if(!rottens[j].length) continue;
+        if(!rottens[j].includes(i)) rottenCheck[i] = false;
+    }
+}
+
+let ans = 0;
+
+for(let i = 1 ; i < rottenCheck.length ; i++){
+    if(!rottenCheck[i]) continue;
+
+    const medicines = eats.filter(eat=>eat[1] === i).length;
+
+    ans = ans > medicines ? ans : medicines
+}
+
+console.log(ans)
