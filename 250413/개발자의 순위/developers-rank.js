ae602abdcb79e72dcac89079c70411af4190c731
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-04-13 16:12:11 +0900

    Title: 개발자의 순위, Time: 202ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 250413/개발자의 순위/developers-rank.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(line=>line.trim().split(' ').map(Number));
+
+const [NUM_OF_GAME, NUM_OF_DEV] = input[0]
+const games = input.slice(1);
+
+let ans = 0;
+
+for(let player1 = 1 ; player1 <= NUM_OF_DEV ; player1++){
+
+    for(let player2 = 1 ; player2 <= NUM_OF_DEV ; player2++){
+        if(player1 === player2) continue;
+
+        let count = 0;
+
+        for(const game of games){
+            
+            const player1_rank = game.findIndex(player => player === player1);
+            const player2_rank = game.findIndex(player => player === player2);
+
+            if(player1_rank < player2_rank) count++;
+        }
+
+        if(count === NUM_OF_GAME) ans++;
+    }
+}
+
+console.log(ans)
