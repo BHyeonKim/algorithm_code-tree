e79f891d29d27ffe9ad4c327ecce7ec3f782a2c0
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-04-15 16:37:16 +0900

    Title: 스승의 은혜 3, Time: 170ms, Memory: 15MB, Status: Passed - Codetree

--- 250415/스승의 은혜 3/the-grace-form-teacher-3.js
+++ 250415/스승의 은혜 3/the-grace-form-teacher-3.js
@@ -5,12 +5,28 @@ const [NUM_OF_STUDENT, BUDGET] = input[0].split(' ').map(Number);
 
 const demands = input.slice(1).map(line=>line.split(' ').map(Number)).sort((a,b)=>((a[0]+a[1])-(b[0]+b[1])));
 
+let ans = 0;
 
-for(let excludeIndex = 0 ; excludeIndex < NUM_OF_STUDENT ; excludeIndex++){
+for(let halfPriceIndex = 0 ; halfPriceIndex < NUM_OF_STUDENT ; halfPriceIndex++){
     let budget = BUDGET;
     let numOfGift = 0;
 
-    const newDemands = [...demands.slice(0, excludeIndex),...demands.slice(excludeIndex+1)];
+    const newDemands = [...demands];
+    newDemands[halfPriceIndex] = [newDemands[halfPriceIndex][0]/2,newDemands[halfPriceIndex][1]]
+    newDemands.sort((a,b)=>((a[0]+a[1])-(b[0]+b[1])))
 
-    console.log(newDemands)
+
+    for(let i = 0 ; i < NUM_OF_STUDENT - 1 ; i++){
+        const [giftPrice, shipPrice] = newDemands[i];
+
+        budget -= giftPrice+shipPrice;
+
+        if(budget < 0) break;
+
+        numOfGift++;
+    }
+
+    ans = ans > numOfGift ? ans : numOfGift
 }
+
+console.log(ans)
