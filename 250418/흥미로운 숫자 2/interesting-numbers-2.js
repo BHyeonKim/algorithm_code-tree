14ad08d11bee18c70e3b8822254d34398f97e42a
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-04-18 01:49:47 +0900

    Title: 흥미로운 숫자 2, Time: 204ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250418/흥미로운 숫자 2/interesting-numbers-2.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const [start, end] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+
+let ans = 0;
+
+for(let num = start ; num <= end ; num++){
+    let num1;
+    let num2;
+
+    let numOfNum1 = 0;
+    let numOfNum2 = 0;
+
+    const numArr = String(num).split('').map(Number);
+
+    for(const val of numArr){
+        if(num1 === val){
+            numOfNum1++
+            continue;
+        }else if(num2 === val){
+            numOfNum2++
+            continue;
+        }
+
+        if(!num1){
+            num1 = val;
+            numOfNum1++;
+            continue;
+        }else if(!num2){
+            num2 = val;
+            numOfNum2++;
+            continue;
+        }
+
+        // 위의 조건에 하나도 해당하지 않으면 break;
+        break;
+    }
+
+    if(numOfNum1 + numOfNum2 === numArr.length && (numOfNum1 === numArr.length - 1 || numOfNum2 === numArr.length - 1)) ans++
+}
+
+console.log(ans)
