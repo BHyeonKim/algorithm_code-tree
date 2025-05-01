23fb77986bd3c7f2925ab39f12a88ff417d2e303
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-05-02 08:30:35 +0900

    Title: 수들의 최대 차, Time: 219ms, Memory: 17MB, Status: Passed - Codetree

--- 250502/수들의 최대 차/maximum-difference-in-numbers.js
+++ 250502/수들의 최대 차/maximum-difference-in-numbers.js
@@ -1,45 +1,32 @@
 const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-const [N, K] = input[0].split(' ')
+const [N, K] = input[0].split(' ').map(Number)
 const numbers = input.slice(1).map(Number)
 
-const vistied = Array(numbers.length).fill(false);
+const vistied = Array(N).fill(false);
 
 let ans = 0;
 
-for(let index = 0 ; index < numbers.length ; index++){
-    vistied[index] = true;
-    dfs(index, 0)
-    vistied[index] = false;
+for(let i = 0 ; i < N ; i++){
+    const num = numbers[i]
+
+    const arrR = []
+    const arrL = []
+
+    const leftBound = num - K;
+
+    const rightBound = num + K;
+
+    for(let j = 0 ; j < N ; j++){
+        const n = numbers[j]
+
+        if(num <= n && n <= rightBound) arrR.push(n);
+        if(leftBound <= n && n <= num) arrL.push(n);
+    }
+
+    ans = Math.max(ans, arrR.length, arrL.length)
 }
 
-
-function dfs(i ,currentDepth){
-
-    if(currentDepth > ans){
-        ans = currentDepth
-    }
-
-    if(currentDepth === numbers.length){
-        return;
-    }
-
-    const num = numbers[i];
-
-
-    for(let j = 0 ; j < numbers.length ; j++){
-        if(vistied[j]) continue;
-
-        const nextNum = numbers[j]
-
-        if(Math.abs(num - nextNum) > K) continue;
-
-        vistied[j] = true;
-        dfs(j, currentDepth+1);
-        vistied[j] = false;
-
-    }
-}
 
 console.log(ans)
