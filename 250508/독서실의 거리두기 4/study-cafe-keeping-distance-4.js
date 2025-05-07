dede68f56353e4fd0ead85ca79cb154ef326c020
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-05-08 08:05:56 +0900

    Title: 독서실의 거리두기 4, Time: 179ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250508/독서실의 거리두기 4/study-cafe-keeping-distance-4.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const N = +input[0]
+const arr = input[1].split('').map(Number);
+
+let ans = 0;
+
+for(let i = 0 ; i < N ; i++){
+    if(arr[i]) continue;
+
+    arr[i] = 1;
+
+    for(let j = 0 ; j < N ; j++){
+        if(arr[j]) continue;
+
+        let distance = 100;
+        let prev;
+        arr[j] = 1;
+
+        for(let k = 0 ; k < N ; k++){
+            if(!prev && arr[k]){
+                prev = k;
+                continue;
+            }
+
+            if(arr[k]){
+                distance = Math.min(distance, k - prev);
+                prev = k;
+            }
+        }
+
+        ans = Math.max(ans, distance)
+
+        arr[j] = 0
+    }
+
+    arr[i] = 0;
+}
+
+console.log(ans)
