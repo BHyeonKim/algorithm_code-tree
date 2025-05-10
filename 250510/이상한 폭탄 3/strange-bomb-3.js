72d399f249c16287de08e1b0372393d7cdb16097
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-05-10 23:13:58 +0900

    Title: 이상한 폭탄 3, Time: 174ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250510/이상한 폭탄 3/strange-bomb-3.js
+++ 250510/이상한 폭탄 3/strange-bomb-3.js
@@ -3,24 +3,41 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [N, K] = input[0].split(' ').map(Number);
 const bombs = input.slice(1).map(Number);
+const visited = Array(N).fill(false);
+
+const map = new Map()
 
 let max = 0
 let ans = 0;
 
 for(let i = 0 ; i < N ; i++){
+    if(visited[i]) continue;
+
     const bomb = bombs[i]
     const exploded = explode(i, bomb, 0)
 
-    if(exploded > max){
-        max = exploded;
-        ans = bomb
-    }else if(exploded === max){
-        ans = ans > bomb ? ans : bomb
+
+
+    if(map.has(bomb)){
+        map.set(bomb, map.get(bomb)+exploded)
+    }else{
+        map.set(bomb, exploded)
     }
 }
 
-console.log(ans)
+let ansBomb = 0;
+let ansCount = 0;
 
+for(const [bombNumber, explodedCount] of map.entries()){
+    if(explodedCount > ansCount){
+        ansCount = explodedCount;
+        ansBomb = bombNumber
+    }else if(explodedCount === ansCount){
+        ansBomb = ansBomb > bombNumber ? ansBomb : bombNumber
+    }
+}
+
+console.log(ansBomb)
 
 function explode(index, bomb, count){
 
@@ -31,10 +48,12 @@ function explode(index, bomb, count){
 
         for(let i = index ; i <= index+K && i < N ; i++){
             if(bombs[i] === bomb){
+                visited[i] = true;
                 currentCount++;
                 lastIndex = i;
             }
         }
+
 
         if(lastIndex !== -1){
             return explode(lastIndex+1, bomb, currentCount)
