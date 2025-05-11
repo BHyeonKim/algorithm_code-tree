a247d3a2824337806d11c2bcfc2bbbb910451528
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-05-12 08:27:37 +0900

    Title: 구간 잘 나누기, Time: 1292ms, Memory: 160MB, Status: Memory Limit Exceed - Codetree

---
+++ 250512/구간 잘 나누기/divide-sections-well.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [N, M] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+const combinations = [];
+
+function dfs(start, acc) {
+    if (acc.length === M - 1) {
+        combinations.push([...acc]);
+        return;
+    }
+
+    for (let i = start; i < N - 1; i++) {
+        acc.push(i);
+        dfs(i + 1, acc);
+        acc.pop();
+    }
+}
+
+let ans = Number.MAX_SAFE_INTEGER;
+
+dfs(0, []);
+
+for(const split of combinations){
+
+    const maxArr = []
+
+    maxArr.push(arr.slice(0, split[0]+1).reduce((acc,cur)=>acc+=cur,0))
+
+    for(let i = 0 ; i < split.length - 1 ; i++){
+        const splitStart = split[i];
+        const splitEnd = split[i+1];
+
+        maxArr.push(arr.slice(splitStart+1, splitEnd + 1).reduce((acc,cur)=>acc+=cur,0))
+    }
+
+    maxArr.push(arr.slice(split[split.length-1]+1).reduce((acc,cur)=>acc+=cur,0))
+
+    const max = Math.max(...maxArr)
+
+    ans = ans > max ? max : ans
+}
+
+console.log(ans)
