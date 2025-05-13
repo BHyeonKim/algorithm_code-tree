c16cbf67b3c7778125be2bb7ccea8e34ffe3b52b
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-05-14 08:25:08 +0900

    Title: 두 직사각형, Time: 195ms, Memory: 15MB, Status: Passed - Codetree

--- 250514/두 직사각형/two-rectangles.js
+++ 250514/두 직사각형/two-rectangles.js
@@ -4,14 +4,8 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [x1, y1, x2, y2] = input[0].split(' ').map(Number);
 const [a1, b1, a2, b2] = input[1].split(' ').map(Number);
 
-// (x1,y1).   (x2, y2)           (a1, b1).   (a2, b2)
-//
-//
-// (x1,y2).   (x2, y2)           (a1, b2).   (a2, b2)
-
-
-if(x1 <= a2 && y1 <= b2 ||  a1 <= x2 && b1 <= y2 || x2 >= a1 && y2 <= b2 || x1 <= a2 && b2 <= y2){
-    console.log('overlapping')
-}else{
-    console.log(nonoverlapping)
+if (!(x2 < a1 || a2 < x1 || y2 < b1 || b2 < y1)) {
+    console.log('overlapping');
+} else {
+    console.log('nonoverlapping');
 }
