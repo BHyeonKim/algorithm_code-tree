f9c492e30f4c133689998faa0603c20ee5e57965
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-05-20 08:50:40 +0900

    Title: 코딩톡, Time: 200ms, Memory: 20MB, Status: Wrong Answer - Codetree

---
+++ 250520/코딩톡/coding-talk.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [NUM_OF_PERSON, NUM_OF_MESSAGE, MESSAGE_NUMBER] = input[0].split(' ').map(Number);
+const messages = input.slice(1).map(line => line.split(' ').map((item,index)=>index === 1 ? Number(item) : item));
+
+const map = new Map();
+
+for(let charcode = 65 ; charcode < 65 + NUM_OF_PERSON ; charcode++){
+    map.set(String.fromCharCode(charcode), false)
+}
+
+for(let i = MESSAGE_NUMBER - 1 ; i < NUM_OF_MESSAGE ; i++){
+    const [person] = messages[i];
+
+    map.set(person, true);
+}
+
+let ans = ''
+
+for(const [key,value] of map.entries()){
+    if(!value){
+        ans += key + ' '
+    }
+}
+
+console.log(ans.trim())
