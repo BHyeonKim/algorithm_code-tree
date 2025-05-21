2bab2fcacaa4970203641bdefe2645b05feeb74a
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-05-22 08:45:03 +0900

    Title: 순 간 이 동, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

--- 250522/순 간 이 동/teleportation.js
+++ 250522/순 간 이 동/teleportation.js
@@ -3,11 +3,13 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [a, b, x, y] = input[0].split(' ').map(Number);
 
+const A = Math.min(a,b);
+const B = Math.max(a,b)
 const X = Math.min(x,y);
 const Y = Math.max(x,y);
 
 const distance1 = Math.abs(a-b);
 
-const distance2 = Math.abs(a-X) + Math.abs(b-Y)
+const distance2 = Math.abs(A-X) + Math.abs(B-Y)
 
 console.log(Math.min(distance1, distance2))
