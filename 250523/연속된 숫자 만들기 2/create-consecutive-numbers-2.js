9fe7734ec0011a978d6188da3d4ae5b68ed540b6
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-05-23 09:15:47 +0900

    Title: 연속된 숫자 만들기 2, Time: 185ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250523/연속된 숫자 만들기 2/create-consecutive-numbers-2.js
+++ 250523/연속된 숫자 만들기 2/create-consecutive-numbers-2.js
@@ -1,41 +1,18 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
-let arr = input[0].split(" ").map(Number);
-
-let count = 0;
-
-while(!isContinuous(...arr)){
-    arr.sort((a,b)=>a-b)
-    const distA = arr[1] - arr[0]
-    const distB = arr[2] - arr[1]
-    if(distA > distB && distB >= 2){
-        arr[0] = arr[2] - 1;
-    }else if(distA > distB){
-        arr[2] = arr[1] - 1
-    }else if(distA < distB && distA >= 2){
-        arr[2] = arr[1] - 1;
-    }else if(distA < distB){
-        arr[0] = arr[2] - 1
-    }
-
-    count++
-}
-
-console.log(count)
+const arr = input[0].split(" ").map(Number);
 
 
+arr.sort((a,b)=>a-b);
 
 
-function isContinuous(...arr){
-    let isContinuous = true;
+const distA = arr[1] - arr[0]
+const distB = arr[2] - arr[2];
 
-    const numbers = [...arr].sort((a,b)=>a-b)
-    for(let i = 0 ; i < numbers.length - 1 ; i++){
-        if(numbers[i] + 1 !== numbers[i+1]){
-            isContinuous = false;
-            break;
-        }
-    }
-
-    return isContinuous;
+if(distA === 1 && distB === 1){
+    console.log(0)
+}else if(distA === 2 || distB === 2){
+    console.log(1)
+}else{
+    console.log(2)
 }
