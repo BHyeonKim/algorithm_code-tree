17925b736a9ea789316ce3ef7d0b7935612ce88e
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-05-25 19:19:36 +0900

    Title: 독서실의 거리두기 3, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

--- 250525/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
+++ 250525/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
@@ -2,8 +2,43 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const N = Number(input[0]);
-const seats = input[1].split("").map(Number);
+const seats = input[1].split("");
+
+let selectedI = -1;
+let selectedJ = -1;
+
+let maxDist = 0;
 
 for(let i = 0 ; i < N - 1 ; i++){
-    for(let )
+    if(seats[i] === '0') continue;
+    
+    for(let j = i + 1 ; j < N ; j++){
+        if(seats[j] === '0') continue;
+
+        if(j - i > maxDist){
+            selectedI = i;
+            selectedJ = j;
+            maxDist = j - i;
+        }
+        break;
+    }
 }
+
+seats[Math.floor((selectedI+selectedJ)/2)] = '1'
+
+let minDist = N
+for(let i = 0 ; i < N - 1 ; i++){
+    if(seats[i] === '0') continue;
+    
+    for(let j = i + 1 ; j < N ; j++){
+        if(seats[j] === '0') continue;
+
+        if(j - i < minDist){
+            minDist = j - i;
+        }
+        break;
+    }
+}
+
+
+console.log(minDist)
