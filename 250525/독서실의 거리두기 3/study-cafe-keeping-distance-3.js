cadefde6293150868ed334d821eebae776a52aa0
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-05-25 19:06:23 +0900

    Title: 독서실의 거리두기 3, Time: 200ms, Memory: 15MB, Status: Runtime Error - Codetree

---
+++ 250525/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const N = Number(input[0]);
+const seats = input[1].split("").map(Number);
+
+for(let i = 0 ; i < N - 1 ; i++){
+    for(let )
+}
