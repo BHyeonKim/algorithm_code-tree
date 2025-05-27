a40e80b80c86a7c0e29297f61cdda95b68d0b3dd
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-05-27 21:10:59 +0900

    Title: 연속된 숫자 만들기 3, Time: 185ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250527/연속된 숫자 만들기 3/create-consecutive-numbers-3.js
+++ 250527/연속된 숫자 만들기 3/create-consecutive-numbers-3.js
@@ -1,9 +1,29 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
-const arr = input[0].split(" ").map(Number);
+let arr = input[0].split(" ").map(Number);
 
 
-console.log(isAligned([1,2,3]))
+let count = 0;
+
+while(!isAligned(arr)){
+    arr.sort((a,b)=>a-b);
+    
+    if(arr[2] - arr[1] > 1 && arr[1] - arr[0] > 1){
+        if(arr[2]-arr[1] > arr[1]-arr[0]){
+            arr[2] = arr[1] - 1
+        }else{
+            arr[0] = arr[2] - 1
+        }
+    }else if(arr[2] - arr[1] > 1){
+        arr[0] = arr[2] - 1;
+    }else if(arr[1] - arr[0] > 1){
+        arr[2] = arr[1] - 1;
+    }
+
+    count++;
+}
+
+console.log(count)
 
 function isAligned(arr){
     const sorted = [...arr].sort((a,b)=>a-b)
