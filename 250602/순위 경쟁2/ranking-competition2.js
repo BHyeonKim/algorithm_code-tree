0d68d580bbcb268469b433352a7bdabbb6ab3fbc
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-06-02 07:55:59 +0900

    Title: 순위 경쟁2, Time: 207ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250602/순위 경쟁2/ranking-competition2.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const N = Number(input[0]);
+const changes = input.slice(1).map(line => line.split(" "));
+
+const score = {
+    'A' : 0,
+    'B' : 0
+}
+
+let honor = ''
+let ans = 0;
+
+for(const [c, s] of changes){
+    score[c] += +s;
+
+    const winner = score['A'] === score['B'] ? 'AB' : score['A'] > score['B'] ? 'A' : 'B'
+    if(winner !== honor){
+        ans++;
+    }
+
+    honor = winner
+}
+
+console.log(ans)
