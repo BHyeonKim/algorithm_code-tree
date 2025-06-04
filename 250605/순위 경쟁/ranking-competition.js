dbb22f4c4733cd94b9ef1affc972b2f6ba7d7e5d
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-06-05 08:21:50 +0900

    Title: 순위 경쟁, Time: 168ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250605/순위 경쟁/ranking-competition.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const N = Number(input[0]);
+const changes = input.slice(1).map(e => e.split(" "));
+
+const score = new Map()
+let max = 0;
+
+score.set('A',0)
+score.set('B',0)
+score.set('C',0)
+
+let ans = 0;
+
+let prev = ['A', 'B', 'C'];
+
+for(const [C, S] of changes){
+    const prevScore = score.get(C);
+    const nextScore = prevScore + Number(S);
+    
+    score.set(C, nextScore);
+
+
+    let maxScore = 0;
+    for(const [_,value] of score.entries()){
+        maxScore = Math.max(maxScore, value)
+    }
+
+    const honor = []
+    for(const [key, value] of score.entries()){
+        if(value === maxScore){
+            honor.push(key)
+        }
+    }
+    
+    if(prev.length !== honor.length){
+        ans++
+    }else{
+        prev.sort()
+        honor.sort()
+
+        for(let i = 0 ; i < prev.length ; prev++){
+            if(prev[i] !== honor[i]){
+                ans++
+                break;
+            }
+        }
+    }
+
+    // console.log('prev:',prev, ' honor:', honor,' ans:', ans)
+    prev = [...honor]
+}
+console.log(ans)
