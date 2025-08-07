c2317cb6b26c4726d9db17485543e2daf8f2040a
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-08-07 22:39:22 +0900

    Title: 세 수의 최대 곱, Time: 163ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250807/세 수의 최대 곱/maximum-product-of-three-numbers.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const N = +input[0]
+const arr = input[1].split(' ').map(Number).sort((a,b)=>a-b);
+
+let ans = 0;
+
+if(arr[0] < 0 && arr[1] < 0){
+    ans = Math.max(arr[0]*arr[1]*arr[N-1])
+}
+
+ans = Math.max(ans, arr[N-1]*arr[N-2]*arr[N-3]);
+
+console.log(ans)
+
