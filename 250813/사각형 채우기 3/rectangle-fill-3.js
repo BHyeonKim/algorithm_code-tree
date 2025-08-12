a412b488bb610044022298a52ed4fab2eac63773
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-08-13 04:03:45 +0900

    Title: 사각형 채우기 3, Time: 170ms, Memory: 15MB, Status: Passed - Codetree

--- 250813/사각형 채우기 3/rectangle-fill-3.js
+++ 250813/사각형 채우기 3/rectangle-fill-3.js
@@ -3,11 +3,16 @@ const N = +fs.readFileSync('/dev/stdin').toString().trim()
 
 const dp = Array(1001).fill(0);
 
+dp[0] = 1;
 dp[1] = 2;
 dp[2] = 7;
 
 for(let i = 3 ; i <= N ; i++){
-    dp[i] = (dp[i-1]*2 + dp[i-2]*4)%1_000_000_007
+    dp[i] = (dp[i-1]*2 + dp[i-2]*3)%1_000_000_007
+
+    for(let j = i - 3 ; j >= 0 ; j--){
+        dp[i] = (dp[i]+ dp[j]*2)%1_000_000_007
+    }
 }
 
 console.log(dp[N])
