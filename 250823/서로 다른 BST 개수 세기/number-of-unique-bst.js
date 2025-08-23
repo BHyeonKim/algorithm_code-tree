f330b3d064632df772c938f672295454596b8a5d
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-08-23 22:12:14 +0900

    Title: 서로 다른 BST 개수 세기, Time: 172ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250823/서로 다른 BST 개수 세기/number-of-unique-bst.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const input = +fs.readFileSync('/dev/stdin').toString().trim();
+
+const memo = Array(20).fill(0)
+
+memo[1] = 1;
+memo[2] = 2;
+memo[3] = 5;
+
+function recursive(n){
+    if(memo[n]) return memo[n]
+    if(n <= 1){
+        return 1
+    }
+
+    let acc = 0;
+    for(let i = 0 ; i < n ; i++){
+        if(!memo[i]){
+            memo[i] = recursive(i) * recursive(n - i - 1);
+        }
+        acc += memo[i]
+    }
+
+    memo[n] = acc
+
+    return memo[n];
+}
+
+
+console.log(recursive(input))
