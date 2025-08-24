ebf135b16964931e6358f0756c8a11784f180985
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-08-25 08:43:00 +0900

    Title: 사각형 채우기 2, Time: 179ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250825/사각형 채우기 2/rectangle-fill-2.js
+++ 250825/사각형 채우기 2/rectangle-fill-2.js
@@ -3,12 +3,14 @@ const N = +fs.readFileSync('/dev/stdin').toString().trim()
 
 const dp = Array(N+1).fill(0);
 
+const DIV_NUM = 10007
+
 dp[1] = 1;
 dp[2] = 3;
 dp[3] = 5;
 
 for(let i = 4 ; i <= N ; i++){
-    dp[i] = dp[i-1] + dp[i-2] * 2
+    dp[i] = (dp[i-1]) % DIV_NUM + (dp[i-2] * 2) % DIV_NUM
 }
 
 console.log(dp[N])
