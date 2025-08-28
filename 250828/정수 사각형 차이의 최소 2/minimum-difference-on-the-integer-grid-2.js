682561e10be76aa9ccca8a55e39f0717d6a09998
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-08-28 15:26:05 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 235ms, Memory: 16MB, Status: Passed - Codetree

--- 250828/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.js
+++ 250828/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.js
@@ -1,45 +1,60 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const MAX = 100
 
 const N = +input[0];
-const map = input.slice(1).map(line => line.split(' ').map(Number));
+const map = input.slice(1).map(line=>line.split(' ').map(Number));
+const dp = Array.from({length:N},()=>Array.from({length:N},()=>MAX));
 
-const dp = Array.from({length: N}, () =>
-    Array.from({length: N}, () =>
-        Array.from({length: 101}, () => Array(101).fill(false))
-    )
-);
+let ans = 100;
 
-dp[0][0][map[0][0]][map[0][0]] = true;
+function initDpTable(){
 
-for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-        for (let min = 1; min <= 100; min++) {
-            for (let max = min; max <= 100; max++) {
-                if (!dp[i][j][min][max]) continue;
+    for(let i = 0 ; i < N ; i++){
+        for(let j = 0 ; j < N ; j++){
+            dp[i][j] = MAX;
+        }
+    }
 
-                const val = map[i][j];
-                const newMin = Math.min(min, val);
-                const newMax = Math.max(max, val);
+    dp[0][0] = map[0][0];
 
-                if (i + 1 < N) {
-                    dp[i+1][j][newMin][newMax] = true;
-                }
+    for(let i = 1 ; i < N ; i++){
+        dp[i][0] = Math.max(dp[i-1][0], map[i][0]);
+    }
 
-                if (j + 1 < N) {
-                    dp[i][j+1][newMin][newMax] = true;
-                }
+    for(let i = 1 ; i < N ; i++){
+        dp[0][i] = Math.max(dp[0][i-1], map[0][i]);
+    }
+}
+
+
+function explore(low){
+    for(let i = 0 ; i < N ; i++){
+        for(let j = 0 ; j < N ; j++){
+            if(map[i][j] < low){
+                map[i][j] = MAX;
             }
         }
     }
-}
 
-let answer = Infinity;
-for (let min = 1; min <= 100; min++) {
-    for (let max = min; max <= 100; max++) {
-        if (dp[N-1][N-1][min][max]) {
-            answer = Math.min(answer, max - min);
+    initDpTable();
+
+    for (let i = 1; i < N; i++) {
+        for (let j = 1; j < N; j++) {
+            dp[i][j] = Math.max(Math.min(dp[i - 1][j], dp[i][j - 1]), map[i][j]);
         }
     }
+
+    return dp[N - 1][N - 1];
 }
-console.log(answer);
+
+for(let low = 1 ; low <= MAX ; low++){
+    const high = explore(low);
+
+    if(high === MAX) continue;
+
+    ans = Math.min(ans, high - low)
+}
+
+console.log(ans);
