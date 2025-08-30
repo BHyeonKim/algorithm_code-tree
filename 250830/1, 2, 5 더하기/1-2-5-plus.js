ba0765cb44d3226020c7d1e5be0e53d2a154b5db
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-08-30 20:56:00 +0900

    Title: 1, 2, 5 더하기, Time: 176ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250830/1, 2, 5 더하기/1-2-5-plus.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const N = +fs.readFileSync('/dev/stdin').toString().trim();
+
+const numbers = [1,2,5];
+
+const dp = Array(N+1).fill(0);
+
+dp[0] = 1;
+dp[1] = 1;
+dp[2] = 2;
+dp[3] = 3;
+
+for(let i = 4 ; i <= N ; i++){
+    for(const n of numbers){
+        if(i >= n){
+            dp[i] += dp[i - n] % 10007;
+        }
+    }
+}
+
+console.log(dp[N])
