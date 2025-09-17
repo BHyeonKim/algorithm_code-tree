e332bc02d8d4aa004cf4f543df607dd155bf7cb0
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-09-17 20:44:54 +0900

    Title: 계단 수, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 250917/계단 수/stair-number.js
+++ 250917/계단 수/stair-number.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const N = +fs.readFileSync(0).toString().trim().split('\n');
 
+const MOD = Math.pow(10,9)+7;
+
 const dp = Array.from({length:1001},()=>Array.from({length:10},()=>0));
 
 for(let i = 1 ; i <= 9 ; i++){
@@ -10,13 +12,13 @@ for(let i = 1 ; i <= 9 ; i++){
 for(let i = 2 ; i <= N ; i++){
     for(let j = 0 ; j <= 9 ; j++){
         if (j === 0){
-            dp[i][j] = dp[i-1][j+1];
+            dp[i][j] = (dp[i-1][j+1])%MOD;
         }else if(j === 9){
-            dp[i][j] = dp[i-1][j-1];
+            dp[i][j] = (dp[i-1][j-1])%MOD;
         }else{
-            dp[i][j] = dp[i-1][j+1] + dp[i-1][j-1];
+            dp[i][j] = (dp[i-1][j+1] + dp[i-1][j-1])%MOD;
         }
     }
 }
 
-console.log(dp[N].reduce((acc,val)=>acc+val,0))
+console.log(dp[N].reduce((acc,val)=>(acc+val)%MOD,0))
