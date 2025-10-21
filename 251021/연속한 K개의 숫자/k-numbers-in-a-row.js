0af43d71b3d1d11f34fe3f74459ed4c37ecbc82e
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-10-21 12:08:31 +0900

    Title: 연속한 K개의 숫자, Time: 4966ms, Memory: 12MB, Status: Wrong Answer - Codetree

--- 251021/연속한 K개의 숫자/k-numbers-in-a-row.js
+++ 251021/연속한 K개의 숫자/k-numbers-in-a-row.js
@@ -2,7 +2,17 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
 const [N, K, B] = input[0].trim().split(" ").map(Number);
-const arr = [0, ...input.slice(1, 1 + B).map(Number)];
+const blankArr = input.slice(1, 1 + B).map(Number);
+
+const arr = Array(N+1).fill(0);
+
+for(let i = 0 ; i < N+1 ; i++){
+    arr[i] = i;
+}
+
+for(const blank of blankArr){
+    arr[blank] = 0;
+}
 
 const sum = Array.from({length:N+1},()=>0);
 
@@ -13,8 +23,6 @@ for(let i = 1 ; i < N+1 ; i++){
 
 
 let ans = Number.MAX_SAFE_INTEGER
-
-console.log(sum)
 
 for(let i = N ; i >= 0 ; i--){
     let blank = 0;
@@ -32,6 +40,5 @@ for(let i = N ; i >= 0 ; i--){
         prev = sum[j]
     }
 }
-
 
 console.log(ans)
