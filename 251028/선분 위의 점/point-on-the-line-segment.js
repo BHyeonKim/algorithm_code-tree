2b04c66adc6d25eb4b58d0a774355703b454a021
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-10-28 17:03:51 +0900

    Title: 선분 위의 점, Time: 351ms, Memory: 49MB, Status: Wrong Answer - Codetree

--- 251028/선분 위의 점/point-on-the-line-segment.js
+++ 251028/선분 위의 점/point-on-the-line-segment.js
@@ -2,22 +2,58 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [N, M] = input[0].split(' ').map(Number);
-const points = input[1].split(' ').map(Number);
+const points = input[1].split(' ').map(Number).sort((a,b)=>a-b);
 const lines = input.slice(2).map(line => line.split(' ').map(Number));
-
 
 let ans = ''
 
-for(const [start, end] of lines){
-    let count = 0;
+for(const[start, end] of lines){
+    const lowerBound = getLowerBound(points, start);
+    const upperBound = getUpperBound(points, end);
 
-    for(const point of points){
-        if(start <= point && point <= end){
-            count++
+    if(lowerBound === -1){
+        ans += '0\n';
+    }else if(upperBound === -1){
+        ans += (N - lowerBound) + '\n';
+    }else{
+        ans += (upperBound - lowerBound) + '\n';
+    }
+}
+
+console.log(ans.trim())
+
+function getUpperBound(arr, target){
+    let left = 0, right = arr.length - 1;
+    let bound = arr.length;
+
+    while(left <= right){
+        const mid = Math.floor((left+right)/2);
+
+        if(arr[mid] > target){
+            bound = Math.min(bound, mid);
+            right = mid - 1;
+        }else{
+            left = mid + 1;
         }
     }
 
-    ans += count + '\n'
+    return bound === arr.length ? -1 : bound;
 }
 
-console.log(ans)
+function getLowerBound(arr, target){
+    let left = 0, right = arr.length - 1;
+    let bound = arr.length;
+
+    while(left <= right){
+        const mid = Math.floor((left+right)/2);
+
+        if(arr[mid] >= target){
+            bound = Math.min(bound, mid);
+            right = mid - 1;
+        }else{
+            left = mid + 1;
+        }
+    }
+
+    return bound === arr.length ? -1 : bound;
+}
