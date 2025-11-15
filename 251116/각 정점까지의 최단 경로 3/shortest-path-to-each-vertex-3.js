41682fdb807e4bbb1e7e04731cee4b0e3d80db46
Bo Hyun Kim <46583212+BHyeonKim@users.noreply.github.com>
2025-11-16 02:08:12 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251116/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [N, M] = input[0].split(" ").map(Number);
+const edges = input.slice(1, M + 1).map(line => line.split(" ").map(Number));
+
+const dp = Array(N+1).fill(Infinity);
+const visited = Array(N+1).fill(false)
+
+dp[1] = 0
+
+const table = Array.from({length:N+1},()=>Array.from({length:N+1},()=>-1))
+
+for(const edge of edges){
+    table[edge[0]][edge[1]] = edge[2]
+}
+
+
+for(let i = 1 ; i <= N ; i++){
+    let minIndex = -1;
+
+    for(let j = 1 ; j <= N ; j++){
+        if(visited[j]) continue;
+
+        if(minIndex === -1 || dp[minIndex] > dp[j]){
+            minIndex = j
+        }
+    }
+
+    visited[minIndex] = true
+
+    for(let j = 1 ; j <= N ; j++){
+        if(table[minIndex][j] === -1) continue;
+
+        dp[j] = Math.min(dp[j], dp[minIndex] + table[minIndex][j])
+    }
+}
+
+
+let ans = ''
+
+for(let i = 2 ; i <= N ; i++){
+    ans += dp[i] + '\n'
+}
+
+console.log(ans.trim())
